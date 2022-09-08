32ab985b3de930410e4f10828fcaaa088e20c3a9
Ashish <ashishjoshi2023@gmail.com>
2022-09-08 06:45:26 +0530

    done

--- c++/linked list/create_node.cpp
+++ c++/linked list/create_node.cpp
@@ -1,14 +1,19 @@
-// Creating A Singley Linked List
+
 #include<iostream>
 using namespace std;
 class Node{
+    public:
     int Data;
     Node *next;
-
-}
+    Node (int Data){
+        this -> Data = Data;
+        this -> next = NULL;
+    }
+};
 int main()
 {
-    Node *n1 = new Node();
-    cout<< n1->
+    Node *n1 = new Node(10);
+    cout<< n1->Data<<endl;
+    cout<< n1->next<<endl;
     return 0;
 }
