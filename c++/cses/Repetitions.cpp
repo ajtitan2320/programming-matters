410cde504e42b261ce4806c749df9829b95546be
Ashish <ashishjoshi2023@gmail.com>
2022-09-12 22:01:13 +0530

    done

--- c++/cses/Repetitions.cpp
+++ c++/cses/Repetitions.cpp
@@ -1,7 +1,24 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-    
+    string s;
+    cin>>s;
+    //sort(s.begin(),s.end());
+    int c=1;
+    int m=0;
+    for(int i=1;i<s.length();i++){
+        if(s[i]==s[i-1]){
+            c++;
+        }
+        else{
+            if(m<c){
+                m=c;
+            }
+            c=1;
+        }
+    }
+    m = max(m,c);
+    cout<<m;
     return 0;
 }
