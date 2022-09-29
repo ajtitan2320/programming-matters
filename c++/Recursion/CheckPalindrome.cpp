26143f7d97e83b18c4dc07cace4049777cdfeff7
Ashish <ashishjoshi2023@gmail.com>
2022-09-29 22:56:18 +0530

    done

---
+++ c++/Recursion/CheckPalindrome.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool CheckPalindrome(string s,int i,int n){
+    if(i>=n/2){
+        return true;
+    }
+    if(s[i]!=s[n-i-1]){
+        return false;
+    }
+    return CheckPalindrome(s,i+1,n);
+}
+int main()
+{
+    string s = "anamana";
+    cout<<CheckPalindrome(s,0,s.length());
+    return 0;
+}
